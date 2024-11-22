10df295c6f0ec9349c8bc446e969110b99d3bab6
TheDevBk <48986470+balarival@users.noreply.github.com>
2024-11-22 21:19:08 +0530

    added coqui api and saved voice

--- TTS_list/Coqui.py
+++ TTS_list/Coqui.py
@@ -1,9 +1,20 @@
 from TTS.api import TTS
 
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
+
 class coquiTTS:
     def __init__(self):
-        self.max_char = 2500
+        self.max_chars = 2500
 
     def run(self, text, filepath, random_voice: bool = False):
+        output_audio_path = r"D:\BK dev\Open Source\RedditVideoMakerBot\assets\temp\1gs924u\mp3\output.wav"
+
+        tts.tts_to_file(
+        text= text,
+        file_path=output_audio_path,
+        speaker_wav=r"D:\BK dev\Open Source\RedditVideoMakerBot\assets\temp\1gs924u\mp3\sample-voice-2.wav",
+        language="en"
+    )
+        print('audio saved')
         
-        pass
+        
