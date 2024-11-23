42f2b5e8e1d6f7a409db48d5b51f2ea93a132ca0
TheDevBk <48986470+balarival@users.noreply.github.com>
2024-11-23 16:50:42 +0530

    coqui voice added, audio issue in final out

--- TTS_list/Coqui.py
+++ TTS_list/Coqui.py
@@ -1,4 +1,9 @@
 from TTS.api import TTS
+import os
+from datetime import datetime
+import hashlib
+
+
 
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
 
@@ -7,14 +12,20 @@ class coquiTTS:
         self.max_chars = 2500
 
     def run(self, text, filepath, random_voice: bool = False):
-        output_audio_path = r"D:\BK dev\Open Source\RedditVideoMakerBot\assets\temp\1gs924u\mp3\output.wav"
+        # Check if the file already exists
+        if os.path.exists(filepath):
+            print(f"File already exists: {filepath}")
+            return
 
+        # Generate the audio file and save it directly to the given filepath
         tts.tts_to_file(
-        text= text,
-        file_path=output_audio_path,
-        speaker_wav=r"D:\BK dev\Open Source\RedditVideoMakerBot\assets\temp\1gs924u\mp3\sample-voice-2.wav",
-        language="en"
-    )
-        print('audio saved')
-        
+            text=text,
+            file_path=filepath,
+            speaker_wav = r"D:\BK dev\Open Source\RedditVideoMakerBot\assets\coqui_sample_voices\sample-voice-2.wav",
+            language="en"
+        )
+        print(f'Audio saved at {filepath}')
+
+
+            
         
